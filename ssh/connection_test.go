cd556af1ad9f9958d90fe87c8adc35b9f42cbc2e
Henry Stanley <hstanley@pivotal.io>
2016-12-01 10:18:21 +0000

    Generate RSA privkey for use in SSH client tests
    [finishes #134922399]

--- ssh/connection_test.go
+++ ssh/connection_test.go
@@ -32,7 +32,7 @@ var _ = Describe("Connection", func() {
 		server.Close()
 	})
 	BeforeEach(func() {
-		privateKey = defaultPrivateKey()
+		privateKey = defaultPrivateKey
 		server = testssh.New(GinkgoWriter)
 		hostName = "127.0.0.1:" + strconv.Itoa(server.Port)
 	})
@@ -232,9 +232,9 @@ var _ = Describe("Connection", func() {
 
 				JustBeforeEach(func() {
 					instance1 = testcluster.NewInstance()
-					instance1.CreateUser("test-user", publicKeyForDocker(defaultPrivateKey()))
+					instance1.CreateUser("test-user", publicKeyForDocker(defaultPrivateKey))
 
-					conn, connErr = ssh.ConnectionCreator(instance1.Address(), "test-user", defaultPrivateKey())
+					conn, connErr = ssh.ConnectionCreator(instance1.Address(), "test-user", defaultPrivateKey)
 					Expect(connErr).NotTo(HaveOccurred())
 
 					stdOut, stdErr, exitCode, runError = conn.StreamStdin(command, reader)
@@ -292,9 +292,6 @@ func publicKeyForDocker(privateKey string) string {
 	return "ssh-rsa " + base64.StdEncoding.EncodeToString(parsedPrivateKey.PublicKey().Marshal())
 }
 
-func defaultPrivateKey() string {
-	return "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEA0Pz7vnb5Ieui9oZinuOh2Fyr6wAuWaJMhAClCMrkk0NeLKcX\n2P1hPRvyL9wqXakFlFb1YPIL0pFu0IgIcOGdHF+QTK7o8K4qyeSOK8Qdsi4xokra\nP89FLcT93NbsE3grqXSE7ENKJHGP68pEwc0gTLD1FZ9O7x1IhihuDjtUflO/juAU\n7b5gycg9cJUtpM97k3i/u0dKvy3qMm14mLx4SRhRlVaBTzmGIuUqtOf3aJj4M8SU\ni3j77RGakCEwMCMAZB9i3eYN1L1Ft66DPjDphsp1Hp1NTPxvhCUZyTsPxboSiCDS\nzfZvTLygeZ88dvB3vldouaR0KHk9lbu1HzrKuQIDAQABAoIBAF7FCgfh/bHDIEA4\nyooQ4biytYc4qswczCPkAvLMxwB8wTzwfOD6bdj/TkEjztZwKkaNdHKE8JWJO742\nodVGii9uqooLmzhhUqgBC/OO2ISPbBSTawsam91YgmJd1+owSWRroUdecEW8da5Q\nKAPWWDpO2KT4fBv0pImp1daAUx2BXJR4PR5WbdA9ql6t/oT1ptLEI6KOixkbKZ/Z\nn1RrSnweuoJQira3IcuUlO/4XD//yuvuFj4rYINUbwpH/auMd9fdQXwZKInS4IBV\nwMajoSWB1hX4+YzLzc+V6R7ub35+RFW2X8g/QUbfLixpgx/SM0MgCd0GFd4Oa21C\nrEpRWAECgYEA9xrKZzG6twO6SkTSgRViwOUyXKCNWygMHYv6gF6rsJ9KOY/7OYv2\nK0MSv9RnWp9Js2fAGH80YZTMOCG2qTd85dOEd7z/DldlX2DkwurL2yyZEOdZGv9W\n89RQbx5ScGWmuuIePuE9BXS0LxGvD+Yt9AtN/smoQeNEdoEmlAnlOwECgYEA2ILt\nnk90q09OXm4B+g1nPD/M7Q99+giX+AWLtSHCkvfClIBxG6Xs0qD88MBTk5MrkHcB\ndaU/YrNmMS3GuTZS8wPZ6xu2DmjE/lX55xXg6QbPNCslXMfvw+URWKnckWDNGut5\n3EkkzMZYLrytTrVkd0Q7aOCcDDH1wNrvyXqWJ7kCgYASfI+d7suAO6gpPELfY2Ey\n+zKsWVqZ8kINx9Yi2nJP0Wr1KX9rC7yL+gWiElr1Haue32kwq/uYPVCV9ne66yrN\n6ugjKSGPyhwMaaxTpMtBh3GgIR66dVXlAgJOfd8/B2vU2WvX2nP9P4DncJQ/RUI0\n2s+n+yA6Za1OjFT9iEv9AQKBgDjx7rdlpITuHemeO2zeG5nwGeD74yFhIz87jiw8\nzeVDvuy5/4XLFUesyfo0S4cT/TBI7JxZsxstniIvLQZHsHd0OtuodTDDA5T1Xf4W\ndgo0HUlWU8RcXcaDOBW+z2F5OVjsOCflIQWu4UChpV9/PAZWbt29va1DcqSfsNOo\nJ1gZAoGAF3x4r4LjeFJVhdAAdxSG6LpSUj3ZsBVn+YCrYgZsNMlmCD6+xnbtw9rZ\nL5UoyIJeGTb89xnWaYqXnMqkGF8c4coKb2CG05a6yC1EwL4DSbCDNvVhniARIStL\ng/e1bAStuIe3sA6ixeA/Z0FzkzqBx7yY9oEtDdvPaUA07IeriQg=\n-----END RSA PRIVATE KEY-----\n"
-}
 func makeScript(scr string) string {
 	file, err := ioutil.TempFile(".", "")
 	Expect(err).NotTo(HaveOccurred())
--- ssh/ssh_suite_test.go
+++ ssh/ssh_suite_test.go
@@ -1,6 +1,11 @@
 package ssh_test
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,3 +16,19 @@ func TestSsh(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Ssh Suite")
 }
+
+var defaultPrivateKey string
+
+var _ = BeforeSuite(func() {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	Expect(err).NotTo(HaveOccurred())
+
+	defaultPrivateKeyBytes := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	)
+
+	defaultPrivateKey = string(defaultPrivateKeyBytes)
+})
