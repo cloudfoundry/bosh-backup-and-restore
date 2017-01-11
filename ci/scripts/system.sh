ebb4ca3ee667ce6a02736d8c55127f81e278745f
Alvaro Perez-Shirley <aperezshirley@pivotal.io>
2017-01-11 17:25:23 +0000

    Revert "Fix installing bosh-cli from vendor"
    This reverts commit 1983050f0140e2a52cad87eaae128d619d008c40.

--- ci/scripts/system.sh
+++ ci/scripts/system.sh
@@ -12,5 +12,9 @@ export PATH=$PATH:$GOPATH/bin
 export BOSH_CERT_PATH=`pwd`/pcf-backup-and-restore-meta/certs/lite-bosh.backup-and-restore.cf-app.com.crt
 export BOSH_GATEWAY_KEY=`pwd`/pcf-backup-and-restore-meta/genesis-bosh/bosh.pem
 
+mkdir -p $GOPATH/src/github.com/cloudfoundry
+cp src/github.com/pivotal-cf/pcf-backup-and-restore/vendor/github.com/cloudfoundry/bosh-cli $GOPATH/src/github.com/cloudfoundry
+go install github.com/cloudfoundry/bosh-cli
+
 cd src/github.com/pivotal-cf/pcf-backup-and-restore
 make sys-test-ci
