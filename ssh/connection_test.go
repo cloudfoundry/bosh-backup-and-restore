7f58c6588c71c2a6dcd6fca67bd58476fb074fb0
Will Pragnell <wpragnell@pivotal.io>
2017-05-23 10:41:43 +0100

    Refactored tests to use testcluster to verify ssh
    [#145693923]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- ssh/connection_test.go
+++ ssh/connection_test.go
@@ -3,13 +3,9 @@ package ssh_test
 import (
 	"bytes"
 	"encoding/base64"
-	"io/ioutil"
-	"os"
-	"strconv"
 
 	"github.com/pivotal-cf/bosh-backup-and-restore/ssh"
 	"github.com/pivotal-cf/bosh-backup-and-restore/testcluster"
-	"github.com/pivotal-cf/bosh-backup-and-restore/testssh"
 	gossh "golang.org/x/crypto/ssh"
 
 	. "github.com/onsi/ginkgo"
@@ -19,105 +15,137 @@ import (
 var _ = Describe("Connection", func() {
 	var conn ssh.SSHConnection
 	var connErr error
-	var server *testssh.Server
+	var hostname string
+	var user string
 	var privateKey string
 
-	var hostName string
+	var instance1 *testcluster.Instance
+
+	BeforeEach(func() {
+		instance1 = testcluster.NewInstance()
+		instance1.CreateUser("test-user", publicKeyForDocker(defaultPrivateKey))
+		privateKey = defaultPrivateKey
+		hostname = instance1.Address()
+		user = "test-user"
+	})
 
 	JustBeforeEach(func() {
-		conn, connErr = ssh.ConnectionCreator(hostName, "admin", privateKey)
-	})
-	AfterEach(func() {
-		server.Close()
-	})
-	BeforeEach(func() {
-		privateKey = defaultPrivateKey
-		server = testssh.New(GinkgoWriter)
-		hostName = "127.0.0.1:" + strconv.Itoa(server.Port)
+		conn, connErr = ssh.ConnectionCreator(hostname, user, privateKey)
 	})
 
-	Describe("Creates an SSH connection", func() {
-		It("does not fail", func() {
+	Describe("Connection Creation", func() {
+		It("Creates an SSH connection succsfully", func() {
 			Expect(connErr).NotTo(HaveOccurred())
 		})
-		It("sshes with the user", func() {
-			Expect(server.LastUser).To(Equal("admin"))
+
+		Describe("fails to connect to the server", func() {
+			BeforeEach(func() {
+				hostname = "laksdjf"
+			})
+			It("fails", func() {
+				Expect(connErr).To(HaveOccurred())
+			})
 		})
-		It("uses the given key to connect", func() {
-			Expect(server.LastKey).To(Equal(publicKey(privateKey)))
+
+		Describe("authorization fails", func() {
+			BeforeEach(func() {
+				user = "foo"
+			})
+			It("fails", func() {
+				Expect(connErr).To(HaveOccurred())
+			})
+
+		})
+
+		Describe("Invalid ssh key", func() {
+			BeforeEach(func() {
+				privateKey = "laksdjf"
+			})
+			It("fails", func() {
+				Expect(connErr).To(HaveOccurred())
+			})
 		})
 	})
 
-	Describe("fails to connect to the server", func() {
-		BeforeEach(func() {
-			hostName = "laksdjf"
-		})
-		It("fails", func() {
-			Expect(connErr).To(HaveOccurred())
-		})
-	})
-
-	Describe("authorization fails", func() {
-		BeforeEach(func() {
-			server.FailAuth = true
-		})
-		It("fails", func() {
-			Expect(connErr).To(HaveOccurred())
-		})
-		It("tires to connect with username and key", func() {
-			Expect(server.LastUser).To(Equal("admin"))
-			Expect(server.LastKey).To(Equal(publicKey(privateKey)))
-		})
-	})
-
-	Describe("Invalid ssh key", func() {
-		BeforeEach(func() {
-			privateKey = "laksdjf"
-		})
-		It("fails", func() {
-			Expect(connErr).To(HaveOccurred())
-		})
-	})
-
-	Describe("username", func() {
+	Describe("Username", func() {
 		It("returns the SSH username", func() {
-			Expect(conn.Username()).To(Equal("admin"))
+			Expect(conn.Username()).To(Equal("test-user"))
 		})
 	})
 
-	Describe("Stream", func() {
+	Describe("StreamStdin", func() {
 		Context("succeeds", func() {
-			var writer *bytes.Buffer
+			var reader *bytes.Buffer
 			var stdErr []byte
+			var stdOut []byte
 			var exitCode int
 			var runError error
 			var command string
+
 			JustBeforeEach(func() {
-				stdErr, exitCode, runError = conn.Stream(command, writer)
+				Expect(connErr).NotTo(HaveOccurred())
+				stdOut, stdErr, exitCode, runError = conn.StreamStdin(command, reader)
 			})
+
 			BeforeEach(func() {
-				writer = bytes.NewBufferString("")
-				command = makeScript(`#!/usr/bin/env sh
-				echo "stdout"
-				echo "stderr" >&2`)
+				reader = bytes.NewBufferString("I am from the reader")
+				command = "cat > /tmp/foo; echo 'here is something on stdout'; echo 'here is something on stderr' >&2"
 			})
+
 			AfterEach(func() {
-				if _, err := os.Stat(command); err == nil {
-					Expect(os.Remove(command)).To(Succeed())
-				}
+				instance1.DieInBackground()
 			})
+
 			It("does not fail", func() {
 				Expect(runError).NotTo(HaveOccurred())
 			})
-			It("writes stdout to the writer", func() {
-				Expect(writer.String()).To(ContainSubstring("stdout"))
+
+			It("reads stdout from the reader", func() {
+				stdout, _, _, _ := conn.Run("cat /tmp/foo")
+				Expect(string(stdout)).To(Equal("I am from the reader"))
 			})
+
+			It("drains stdout", func() {
+				Expect(string(stdOut)).To(ContainSubstring("here is something on stdout"))
+			})
+
 			It("drains stderr", func() {
-				Expect(string(stdErr)).To(ContainSubstring("stderr"))
+				Expect(string(stdErr)).To(ContainSubstring("here is something on stderr"))
 			})
 			It("captures exit code", func() {
 				Expect(exitCode).To(BeZero())
 			})
+		})
+	})
+
+	Describe("Stream", func() {
+		var writer *bytes.Buffer
+		var stdErr []byte
+		var exitCode int
+		var runError error
+		JustBeforeEach(func() {
+			Expect(connErr).NotTo(HaveOccurred())
+			stdErr, exitCode, runError = conn.Stream("/tmp/foo", writer)
+		})
+		BeforeEach(func() {
+			writer = bytes.NewBufferString("")
+			instance1.CreateScript("/tmp/foo", `#!/usr/bin/env sh
+				echo "stdout"
+				echo "stderr" >&2
+				exit 1
+				`)
+		})
+		It("does not fail", func() {
+			Expect(runError).NotTo(HaveOccurred())
+		})
+		It("writes stdout to the writer", func() {
+			Expect(writer.String()).To(ContainSubstring("stdout"))
+		})
+		It("drains stderr", func() {
+			Expect(string(stdErr)).To(ContainSubstring("stderr"))
+		})
+		It("captures exit code", func() {
+			Expect(exitCode).To(Equal(1))
 		})
 	})
 
@@ -128,16 +156,13 @@ var _ = Describe("Connection", func() {
 		var runError error
 		var command string
 		JustBeforeEach(func() {
+			Expect(connErr).NotTo(HaveOccurred())
 			stdOut, stdErr, exitCode, runError = conn.Run(command)
-		})
-		AfterEach(func() {
-			if _, err := os.Stat(command); err == nil {
-				Expect(os.Remove(command)).To(Succeed())
-			}
 		})
 		Context("succeds", func() {
 			BeforeEach(func() {
-				command = makeScript(`#!/usr/bin/env sh
+				command = "/tmp/foo"
+				instance1.CreateScript(command, `#!/usr/bin/env sh
 				echo "stdout"
 				echo "stderr" >&2`)
 
@@ -156,24 +181,12 @@ var _ = Describe("Connection", func() {
 			})
 		})
 
-		Context("fails to create new session", func() {
-			BeforeEach(func() {
-				server.FailSession = true
-			})
-			It("fails", func() {
-				Expect(runError).To(HaveOccurred())
-			})
-		})
-
 		Context("running multiple commands", func() {
-			BeforeEach(func() {
-				command = "ls"
-			})
 
 			It("does not fail", func() {
-				_, _, _, runError1 := conn.Run(command)
-				_, _, _, runError2 := conn.Run(command)
-				_, _, _, runError3 := conn.Run(command)
+				_, _, _, runError1 := conn.Run("ls")
+				_, _, _, runError2 := conn.Run("ls")
+				_, _, _, runError3 := conn.Run("ls")
 
 				Expect(runError1).NotTo(HaveOccurred())
 				Expect(runError2).NotTo(HaveOccurred())
@@ -183,7 +196,8 @@ var _ = Describe("Connection", func() {
 
 		Context("exit code not 0", func() {
 			BeforeEach(func() {
-				command = makeScript(`#!/usr/bin/env sh
+				command = "/tmp/foo"
+				instance1.CreateScript(command, `#!/usr/bin/env sh
 				echo "stdout"
 				echo "stderr" >&2
 				exit 12`)
@@ -205,82 +219,15 @@ var _ = Describe("Connection", func() {
 
 		Context("command not found", func() {
 			BeforeEach(func() {
-				command = "foo bar baz"
+				command = "/tmp/non-existent"
 			})
-
 			It("captures exit code", func() {
 				Expect(exitCode).To(Equal(127))
 			})
 		})
 	})
-})
-
-var _ = Describe("Connection", func() {
-	By("testing against a containerised SSH server", func() {
-		Describe("StreamStdin", func() {
-			Context("succeeds", func() {
-				var reader *bytes.Buffer
-				var stdErr []byte
-				var stdOut []byte
-				var exitCode int
-				var runError error
-				var connErr error
-				var command string
-				var conn ssh.SSHConnection
-				var instance1 *testcluster.Instance
-
-				JustBeforeEach(func() {
-					instance1 = testcluster.NewInstance()
-					instance1.CreateUser("test-user", publicKeyForDocker(defaultPrivateKey))
-
-					conn, connErr = ssh.ConnectionCreator(instance1.Address(), "test-user", defaultPrivateKey)
-					Expect(connErr).NotTo(HaveOccurred())
-
-					stdOut, stdErr, exitCode, runError = conn.StreamStdin(command, reader)
-				})
-
-				BeforeEach(func() {
-					reader = bytes.NewBufferString("I am from the reader")
-					command = "cat > /tmp/foo; echo 'here is something on stdout'; echo 'here is something on stderr' >&2"
-				})
-
-				AfterEach(func() {
-					instance1.DieInBackground()
-				})
-
-				It("does not fail", func() {
-					Expect(runError).NotTo(HaveOccurred())
-				})
-
-				It("reads stdout from the reader", func() {
-					stdout, _, _, _ := conn.Run("cat /tmp/foo")
-					Expect(string(stdout)).To(Equal("I am from the reader"))
-				})
-
-				It("drains stdout", func() {
-					Expect(string(stdOut)).To(ContainSubstring("here is something on stdout"))
-				})
-
-				It("drains stderr", func() {
-					Expect(string(stdErr)).To(ContainSubstring("here is something on stderr"))
-				})
-				It("captures exit code", func() {
-					Expect(exitCode).To(BeZero())
-				})
-			})
-		})
-	})
 
 })
-
-func publicKey(privateKey string) string {
-	parsedPrivateKey, err := gossh.ParsePrivateKey([]byte(privateKey))
-	if err != nil {
-		Fail("Cant parse key")
-	}
-
-	return string(parsedPrivateKey.PublicKey().Marshal())
-}
 
 func publicKeyForDocker(privateKey string) string {
 	parsedPrivateKey, err := gossh.ParsePrivateKey([]byte(privateKey))
@@ -289,15 +236,4 @@ func publicKeyForDocker(privateKey string) string {
 	}
 
 	return "ssh-rsa " + base64.StdEncoding.EncodeToString(parsedPrivateKey.PublicKey().Marshal())
-}
-
-func makeScript(scr string) string {
-	file, err := ioutil.TempFile(".", "")
-	Expect(err).NotTo(HaveOccurred())
-	file.Write([]byte(scr))
-	Expect(file.Chmod(0700)).To(Succeed())
-	file.Close()
-	pwd, err := os.Getwd()
-	Expect(err).NotTo(HaveOccurred())
-	return pwd + "/" + file.Name()
 }
