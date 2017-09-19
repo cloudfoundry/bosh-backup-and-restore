b96755ec47bf118a48ab3ad9520e869f7c7abbf5
Ed Cook <ecook@pivotal.io>
2017-09-19 11:59:59 +0100

    Clean up standalone.DeployedInstance
    [#148736241]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- standalone/deployed_instance.go
+++ standalone/deployed_instance.go
@@ -13,50 +13,50 @@ type DeployedInstance struct {
 	*instance.DeployedInstance
 }
 
-func (d DeployedInstance) Cleanup() error {
-	d.Logger.Info("", "Cleaning up...")
-	if !d.ArtifactDirCreated() {
-		d.Logger.Debug("", "Backup directory was never created - skipping cleanup")
-		return nil
-	}
-
-	stdout, stderr, exitCode, err := d.SSHConnection.Run(fmt.Sprintf("sudo rm -rf %s", orchestrator.ArtifactDirectory))
-	d.Logger.Debug("", "Stdout: %s", string(stdout))
-	d.Logger.Debug("", "Stderr: %s", string(stderr))
-
-	if err != nil {
-		d.Logger.Error("", "Backup artifact clean up failed")
-		return errors.Wrap(err, "standalone.DeployedInstance.Cleanup failed")
-	}
-
-	if exitCode != 0 {
-		return errors.New("Unable to clean up backup artifact")
-	}
-
-	return nil
-}
-
-func (d DeployedInstance) CleanupPrevious() error {
-	d.Logger.Info("", "Cleaning up...")
-
-	stdout, stderr, exitCode, err := d.SSHConnection.Run(fmt.Sprintf("sudo rm -rf %s", orchestrator.ArtifactDirectory))
-	d.Logger.Debug("", "Stdout: %s", string(stdout))
-	d.Logger.Debug("", "Stderr: %s", string(stderr))
-
-	if err != nil {
-		d.Logger.Error("", "Backup artifact clean up failed")
-		return errors.Wrap(err, "standalone.DeployedInstance.Cleanup failed")
-	}
-
-	if exitCode != 0 {
-		return errors.New("Unable to clean up backup artifact")
-	}
-
-	return nil
-}
-
 func NewDeployedInstance(instanceGroupName string, connection ssh.SSHConnection, logger instance.Logger, jobs orchestrator.Jobs, artifactDirCreated bool) DeployedInstance {
 	return DeployedInstance{
 		DeployedInstance: instance.NewDeployedInstance("0", instanceGroupName, "0", artifactDirCreated, connection, logger, jobs),
 	}
+}
+
+func (i DeployedInstance) Cleanup() error {
+	i.Logger.Info("", "Cleaning up...")
+	if !i.ArtifactDirCreated() {
+		i.Logger.Debug("", "Backup directory was never created - skipping cleanup")
+		return nil
+	}
+
+	stdout, stderr, exitCode, err := i.SSHConnection.Run(fmt.Sprintf("sudo rm -rf %s", orchestrator.ArtifactDirectory))
+	i.Logger.Debug("", "Stdout: %s", string(stdout))
+	i.Logger.Debug("", "Stderr: %s", string(stderr))
+
+	if err != nil {
+		i.Logger.Error("", "Backup artifact clean up failed")
+		return errors.Wrap(err, "standalone.DeployedInstance.Cleanup failed")
+	}
+
+	if exitCode != 0 {
+		return errors.New("Unable to clean up backup artifact")
+	}
+
+	return nil
+}
+
+func (i DeployedInstance) CleanupPrevious() error {
+	i.Logger.Info("", "Cleaning up...")
+
+	stdout, stderr, exitCode, err := i.SSHConnection.Run(fmt.Sprintf("sudo rm -rf %s", orchestrator.ArtifactDirectory))
+	i.Logger.Debug("", "Stdout: %s", string(stdout))
+	i.Logger.Debug("", "Stderr: %s", string(stderr))
+
+	if err != nil {
+		i.Logger.Error("", "Backup artifact clean up failed")
+		return errors.Wrap(err, "standalone.DeployedInstance.Cleanup failed")
+	}
+
+	if exitCode != 0 {
+		return errors.New("Unable to clean up backup artifact")
+	}
+
+	return nil
 }
