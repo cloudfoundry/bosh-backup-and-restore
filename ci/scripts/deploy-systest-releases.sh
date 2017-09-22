fc35253a8324427cf69a0c930ae398d0de63ce53
Ed Cook <ecook@pivotal.io>
2017-09-22 13:25:02 +0100

    Add deploy-systest-releases task and script

---
+++ ci/scripts/deploy-systest-releases.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+export BOSH_CLIENT
+export BOSH_CLIENT_SECRET
+
+set -x
+
+cd bbr-systest-releases/redis-test-release
+bosh -n create release --force
+
+bosh -n target $BOSH_HOST
+set +x; bosh login $BOSH_CLIENT $BOSH_CLIENT_SECRET; set -x
+bosh upload release --rebase
+
+bosh -n --ca-cert=../../bosh-backup-and-restore-meta/certs/lite-bosh-uaa.backup-and-restore.cf-app.com.crt \
+target $BOSH_UAA_HOST
+export BOSH_CLIENT_SECRET=$BOSH_UAA_CLIENT_SECRET
+bosh login
+bosh upload release --rebase
+
+cd bbr-systest-releases/lock-ordering-release
+bosh -n create release --force
+
+bosh -n target $BOSH_HOST
+set +x; bosh login $BOSH_CLIENT $BOSH_CLIENT_SECRET; set -x
+bosh upload release --rebase
+
+bosh -n --ca-cert=../../bosh-backup-and-restore-meta/certs/lite-bosh-uaa.backup-and-restore.cf-app.com.crt \
+target $BOSH_UAA_HOST
+export BOSH_CLIENT_SECRET=$BOSH_UAA_CLIENT_SECRET
+bosh login
+bosh upload release --rebase
