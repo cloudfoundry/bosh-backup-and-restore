f5f01a472f78e23a9bb6b6026e754a3788c1063f
Ed Cook <ecook@pivotal.io>
2017-09-22 13:41:32 +0100

    Update deploy-systest-release to take RELEASE_NAME

--- ci/scripts/deploy-systest-release.sh
+++ ci/scripts/deploy-systest-release.sh
@@ -7,20 +7,7 @@ export BOSH_CLIENT_SECRET
 
 set -x
 
-cd bbr-systest-releases/redis-test-release
-bosh -n create release --force
-
-bosh -n target $BOSH_HOST
-set +x; bosh login $BOSH_CLIENT $BOSH_CLIENT_SECRET; set -x
-bosh upload release --rebase
-
-bosh -n --ca-cert=../../bosh-backup-and-restore-meta/certs/lite-bosh-uaa.backup-and-restore.cf-app.com.crt \
-target $BOSH_UAA_HOST
-export BOSH_CLIENT_SECRET=$BOSH_UAA_CLIENT_SECRET
-bosh login
-bosh upload release --rebase
-
-cd ../lock-ordering-release
+cd bbr-systest-releases/${RELEASE_NAME}
 bosh -n create release --force
 
 bosh -n target $BOSH_HOST
