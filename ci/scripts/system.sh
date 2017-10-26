05208d57a2d683cb3ecb4a17d8ce1e90a83db39f
Henry Stanley <hstanley@pivotal.io>
2017-10-26 13:49:14 +0100

    Inject BOSH gateway host as a different machine from BOSH director
    [#152311285]

--- ci/scripts/system.sh
+++ ci/scripts/system.sh
@@ -7,7 +7,7 @@ eval "$(ssh-agent)"
 chmod 400 bosh-backup-and-restore-meta/keys/github
 chmod 400 bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
 ssh-add bosh-backup-and-restore-meta/keys/github
-export BOSH_GATEWAY_HOST=$BOSH_HOST
+export BOSH_GATEWAY_HOST
 export BOSH_URL=https://$BOSH_HOST
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
