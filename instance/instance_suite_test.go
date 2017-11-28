61d8df4cd7eb9e63bdead0cd7069e5582d7b2737
Pivotal <pivotal@piv-ws-Belmont.local>
2017-11-28 11:25:10 +0000

    Revert "Add tests for most RemoteRunner methods"
    This reverts commit f1c92cd793b45351face1b9ee7289c8e90a98f1d.

--- instance/instance_suite_test.go
+++ instance/instance_suite_test.go
@@ -4,47 +4,10 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
 	"testing"
-
-	"github.com/cloudfoundry-incubator/bosh-backup-and-restore/testcluster"
-	"golang.org/x/crypto/ssh"
 )
 
 func TestInstance(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Instance Suite")
 }
-
-var defaultPrivateKey string
-
-var _ = BeforeSuite(func() {
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	Expect(err).NotTo(HaveOccurred())
-
-	defaultPrivateKeyBytes := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(key),
-		},
-	)
-
-	defaultPrivateKey = string(defaultPrivateKeyBytes)
-})
-
-func publicKeyForDocker(privateKey string) string {
-	parsedPrivateKey, err := ssh.ParsePrivateKey([]byte(privateKey))
-	if err != nil {
-		Fail("Cant parse key")
-	}
-
-	return "ssh-rsa " + base64.StdEncoding.EncodeToString(parsedPrivateKey.PublicKey().Marshal())
-}
-
-var _ = AfterSuite(func() {
-	testcluster.WaitForContainersToDie()
-})
