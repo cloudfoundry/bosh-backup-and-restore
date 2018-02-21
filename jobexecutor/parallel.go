0dac4bab25a92e4e298b4231b724448bad0c90ee
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-02-21 17:27:02 +0000

    Remove redundant WaitGroup in the ParallelJobExecutor
    [#153632732]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- jobexecutor/parallel.go
+++ jobexecutor/parallel.go
@@ -2,7 +2,6 @@ package jobexecutor
 
 import (
 	"github.com/cloudfoundry-incubator/bosh-backup-and-restore/orchestrator"
-	"sync"
 )
 
 func NewParallelJobExecutor() ParallelJobExecutor {
@@ -15,21 +14,16 @@ type ParallelJobExecutor struct {
 func (s ParallelJobExecutor) Run(runMethod func(orchestrator.Job) error, jobs [][]orchestrator.Job) []error {
 	var errors []error
 	for _, jobList := range jobs {
-		var wg sync.WaitGroup
 		errs := make(chan error, len(jobList))
 
 		for _, job := range jobList {
-			wg.Add(1)
 			go func(j orchestrator.Job) {
-				defer wg.Done()
-				err := runMethod(j)
-				errs <- err
+				errs <- runMethod(j)
 			}(job)
 		}
-		wg.Wait()
-		close(errs)
 
-		for err := range errs {
+		for range jobList {
+			err := <- errs
 			if err != nil {
 				errors = append(errors, err)
 			}
