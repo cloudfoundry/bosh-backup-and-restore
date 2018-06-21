a4a918087d1fb0243fb3fa3933c9d5937506bdb2
Josh Hill <jhill@pivotal.io>
2018-06-21 15:08:14 +0100

    Update deployment system to work with BOSH_ALL_PROXY
    [#158423237]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- ci/scripts/system.sh
+++ ci/scripts/system.sh
@@ -2,18 +2,28 @@
 
 set -eu
 
-eval "$(ssh-agent)"
-chmod 400 bosh-backup-and-restore-meta/keys/github
-chmod 400 "${BOSH_GATEWAY_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}"
-ssh-add bosh-backup-and-restore-meta/keys/github
+if [[ "$USE_BOSH_ALL_PROXY" = true ]]; then
+  echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+  chmod 0600 "${PWD}/ssh.key"
+  export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+  export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
+else
+  eval "$(ssh-agent)"
+  chmod 400 bosh-backup-and-restore-meta/keys/github
+  chmod 400 "${BOSH_GATEWAY_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}"
+  ssh-add bosh-backup-and-restore-meta/keys/github
 
-export BOSH_GATEWAY_HOST=$BOSH_HOST
-export BOSH_URL=https://$BOSH_HOST
+  export BOSH_GATEWAY_HOST=$BOSH_HOST
+  export BOSH_URL=https://$BOSH_HOST
+  export BOSH_GATEWAY_USER=${BOSH_GATEWAY_USER:-vcap}
+  export BOSH_GATEWAY_KEY=$PWD/${BOSH_GATEWAY_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}
+fi
+
+if [[ -z "$BOSH_CA_CERT" ]]; then
+  export BOSH_CERT_PATH=$PWD/bosh-backup-and-restore-meta/certs/$BOSH_HOST.crt
+fi
+
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
-export BOSH_GATEWAY_USER=${BOSH_GATEWAY_USER:-vcap}
-export BOSH_CERT_PATH=$PWD/bosh-backup-and-restore-meta/certs/$BOSH_HOST.crt
-export BOSH_GATEWAY_KEY=$PWD/${BOSH_GATEWAY_KEY:-bosh-backup-and-restore-meta/genesis-bosh/bosh.pem}
-
 cd src/github.com/cloudfoundry-incubator/bosh-backup-and-restore
 make sys-test-ci
