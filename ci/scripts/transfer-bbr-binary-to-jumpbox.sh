d24b988f883d225077ffb0c03972b4c1e168a9aa
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-07-05 11:51:29 +0100

    Fix transfer-bbr-binary-to-jumpbox task
    Make it use the new bosh cli
    
    [#156724232]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- ci/scripts/transfer-bbr-binary-to-jumpbox.sh
+++ ci/scripts/transfer-bbr-binary-to-jumpbox.sh
@@ -5,47 +5,49 @@ set -o pipefail
 
 chmod 400 bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
 
-bosh \
-  -n \
-  -t "${BOSH_TARGET}" \
-  -u "${BOSH_CLIENT}" \
-  -p "${BOSH_CLIENT_SECRET}" \
-  -d bosh-backup-and-restore-meta/deployments/acceptance-jump-box.yml \
+bosh-cli \
+  --non-interactive \
+  --ca-cert="bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt" \
+  --environment="${BOSH_TARGET}" \
+  --client="${BOSH_CLIENT}" \
+  --client-secret="${BOSH_CLIENT_SECRET}" \
+  --deployment="acceptance-jump-box" \
   ssh \
-  --gateway_identity_file bosh-backup-and-restore-meta/genesis-bosh/bosh.pem \
-  --gateway_user vcap \
-  --gateway_host genesis-bosh.backup-and-restore.cf-app.com \
-  jump-box 0 \
-  "sudo mkdir -p /var/vcap/store/bbr && \
-   sudo chmod 775 /var/vcap/store/bbr && \
-   sudo chown vcap:vcap /var/vcap/store/bbr
-  "
+  --gw-private-key="bosh-backup-and-restore-meta/genesis-bosh/bosh.pem" \
+  --gw-user="vcap" \
+  --gw-host="genesis-bosh.backup-and-restore.cf-app.com" \
+  --command="sudo mkdir -p /var/vcap/store/bbr && sudo chmod 775 /var/vcap/store/bbr && sudo chown vcap:vcap /var/vcap/store/bbr" \
+  jump-box
 
 # shellcheck disable=SC2011
-ls rc/bbr* | xargs -INAME bosh -n -t "${BOSH_TARGET}" -u "${BOSH_CLIENT}" -p "${BOSH_CLIENT_SECRET}" \
-  -d bosh-backup-and-restore-meta/deployments/acceptance-jump-box.yml \
-  scp jump-box 0 NAME /var/vcap/store/bbr/ \
-  --upload \
-  --gateway_identity_file bosh-backup-and-restore-meta/genesis-bosh/bosh.pem \
-  --gateway_user vcap \
-  --gateway_host genesis-bosh.backup-and-restore.cf-app.com
+ls rc/bbr* | xargs -INAME bosh-cli \
+  --non-interactive \
+  --ca-cert="bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt" \
+  --environment="${BOSH_TARGET}" \
+  --client="${BOSH_CLIENT}" \
+  --client-secret="${BOSH_CLIENT_SECRET}" \
+  --deployment="acceptance-jump-box" \
+  scp NAME jump-box:/var/vcap/store/bbr/ \
+  --gw-private-key="bosh-backup-and-restore-meta/genesis-bosh/bosh.pem" \
+  --gw-user="vcap" \
+  --gw-host="genesis-bosh.backup-and-restore.cf-app.com"
 
 # shellcheck disable=SC2011
-# list tarballs, remove filename extention, bosh ssh commands to extract tarball
+# list tarballs, remove filename extension, bosh ssh commands to extract tarball
 ls rc/bbr* | \
   xargs -INAME basename NAME | rev | cut -d "." -f2- | rev | \
-  xargs -INAME bosh \
-  -n \
-  -t "${BOSH_TARGET}" \
-  -u "${BOSH_CLIENT}" \
-  -p "${BOSH_CLIENT_SECRET}" \
-  -d bosh-backup-and-restore-meta/deployments/acceptance-jump-box.yml \
+  xargs -INAME bosh-cli \
+  --non-interactive \
+  --ca-cert="bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt" \
+  --environment="${BOSH_TARGET}" \
+  --client="${BOSH_CLIENT}" \
+  --client-secret="${BOSH_CLIENT_SECRET}" \
+  --deployment="acceptance-jump-box" \
   ssh \
-  --gateway_identity_file bosh-backup-and-restore-meta/genesis-bosh/bosh.pem \
-  --gateway_user vcap \
-  --gateway_host genesis-bosh.backup-and-restore.cf-app.com \
-  jump-box 0 \
-  "sudo chpst -u vcap:vcap mkdir -p /var/vcap/store/bbr/NAME && \
-   sudo chpst -u vcap:vcap tar xvf /var/vcap/store/bbr/NAME.tar -C /var/vcap/store/bbr/NAME/ --strip-components 1 && \
-   sudo rm -f /var/vcap/store/bbr/NAME.tar
-  "
+  --gw-private-key="bosh-backup-and-restore-meta/genesis-bosh/bosh.pem" \
+  --gw-user="vcap" \
+  --gw-host="genesis-bosh.backup-and-restore.cf-app.com" \
+  --command="sudo chpst -u vcap:vcap mkdir -p /var/vcap/store/bbr/NAME && \
+    sudo chpst -u vcap:vcap tar xvf /var/vcap/store/bbr/NAME.tar -C /var/vcap/store/bbr/NAME/ --strip-components 1 && \
+    sudo rm -f /var/vcap/store/bbr/NAME.tar" \
+  jump-box
