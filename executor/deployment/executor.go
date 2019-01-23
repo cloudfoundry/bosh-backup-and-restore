d154417e373896b33fc53dc28e41825c4cee2281
Jake Klein <jklein@pivotal.io>
2019-01-23 13:47:42 +0000

    Remove go generate directives for unused fakes
    [#162305294]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- executor/deployment/executor.go
+++ executor/deployment/executor.go
@@ -1,11 +1,9 @@
 package deployment
 
-//go:generate counterfeiter -o fakes/fake_executor.go . Executor
 type DeploymentExecutor interface {
 	Run([]Executable) []DeploymentError
 }
 
-//go:generate counterfeiter -o fakes/fake_executable.go . Executable
 type Executable interface {
 	Execute() DeploymentError
 }
