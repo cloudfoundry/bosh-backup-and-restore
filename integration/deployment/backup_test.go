4a6a0caa8343fe8253fa8145ecab086d6fd85f48
Glen Rodgers <grodgers@pivotal.io>
2019-07-09 11:36:30 +0100

    Prefer fprintln over WriteString, fix answer to no
    [#164492754]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- integration/deployment/backup_test.go
+++ integration/deployment/backup_test.go
@@ -186,7 +186,7 @@ printf "backupcontent2" > $BBR_ARTIFACT_DIRECTORY/backupdump2
 							Expect(string(session.Out.Contents())).To(HaveSuffix("[yes/no]\n"))
 						})
 
-						io.WriteString(stdin, "yes\n")
+						fmt.Fprintln(stdin, "yes")
 
 						By("then exiting with a failure", func() {
 							Eventually(session, 10).Should(gexec.Exit(1))
@@ -213,7 +213,7 @@ printf "backupcontent2" > $BBR_ARTIFACT_DIRECTORY/backupdump2
 							Expect(string(session.Out.Contents())).To(HaveSuffix("[yes/no]\n"))
 						})
 
-						io.WriteString(stdin, "yes\n")
+						fmt.Fprintln(stdin, "no")
 
 						By("waiting for the backup to finish successfully", func() {
 							Eventually(session, 10).Should(gexec.Exit(0))
