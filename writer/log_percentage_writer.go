10cc6ff4ec8bbc563f9ee7fed26f7a90f46f8d97
Gareth Smith <gsmith@pivotal.io>
2020-01-13 16:53:39 +0000

    Make the LogPercentageWriter log percentages of what's written
    [#163736175]
    
    Signed-off-by: Sarah Connor <sconnor@pivotal.io>

--- writer/log_percentage_writer.go
+++ writer/log_percentage_writer.go
@@ -3,34 +3,37 @@ package writer
 import (
 	"io"
 	"sync"
+
+	"github.com/cloudfoundry-incubator/bosh-backup-and-restore/orchestrator"
 )
 
 type LogPercentageWriter struct {
-	Writer  io.Writer
-	mutex   sync.RWMutex
-	counter int
+	Writer    io.Writer
+	mutex     sync.RWMutex
+	counter   int
+	logger    orchestrator.Logger
+	totalSize int
+	command   string
+	message   string
 }
 
-func NewLogPercentageWriter(w io.Writer) *LogPercentageWriter {
-	return &LogPercentageWriter{Writer: w}
+func NewLogPercentageWriter(writer io.Writer, logger orchestrator.Logger, totalSize int, command, message string) *LogPercentageWriter {
+	return &LogPercentageWriter{
+		Writer:    writer,
+		logger:    logger,
+		totalSize: totalSize,
+		command:   command,
+		message:   message,
+	}
 }
 
-func (c *LogPercentageWriter) Write(b []byte) (int, error) {
-	n, err := c.Writer.Write(b)
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+func (l *LogPercentageWriter) Write(b []byte) (int, error) {
+	n, err := l.Writer.Write(b)
 	if err != nil {
 		return 0, err
 	}
 
-	c.counter += n
+	l.counter += n
+	l.logger.Info(l.command, l.message, ((100 * l.counter) / l.totalSize))
 	return n, nil
-}
-
-func (c *LogPercentageWriter) Count() int {
-	var localCounter int
-	c.mutex.Lock()
-	localCounter = c.counter
-	c.mutex.Unlock()
-	return localCounter
 }
--- writer/log_percentage_writer_test.go
+++ writer/log_percentage_writer_test.go
@@ -1,43 +1,39 @@
 package writer_test
 
 import (
-	"bytes"
-	"errors"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	orchestratorFakes "github.com/cloudfoundry-incubator/bosh-backup-and-restore/orchestrator/fakes"
 	"github.com/cloudfoundry-incubator/bosh-backup-and-restore/writer"
 	"github.com/cloudfoundry-incubator/bosh-backup-and-restore/writer/fakes"
 )
 
 //go:generate counterfeiter -o fakes/fake_writer.go io.Writer
 
-var _ = Describe("Writer", func() {
-	It("returns the amount written", func() {
-		backingWriter := bytes.NewBuffer([]byte(""))
-		logPercentageWriter := writer.NewLogPercentageWriter(backingWriter)
+var _ = Describe("LogPercentageWriter", func() {
+	Context("when the total size is 3 and the writer writes 1 at a time", func() {
+		var fakeLogger *orchestratorFakes.FakeLogger
+		var fakeWriter *fakes.FakeWriter
+		var logPercentageWriter *writer.LogPercentageWriter
 
-		n, err := logPercentageWriter.Write([]byte("four"))
-		Expect(err).NotTo(HaveOccurred())
-		Expect(n).To(Equal(4))
+		BeforeEach(func() {
+			fakeLogger = new(orchestratorFakes.FakeLogger)
+			fakeWriter = new(fakes.FakeWriter)
+			fakeWriter.WriteReturns(1, nil)
+			logPercentageWriter = writer.NewLogPercentageWriter(fakeWriter, fakeLogger, 3, "schblam", "message")
+		})
 
-		Expect(logPercentageWriter.Count()).To(Equal(4))
-
-		_, err = logPercentageWriter.Write([]byte("four"))
-		Expect(err).NotTo(HaveOccurred())
-		Expect(logPercentageWriter.Count()).To(Equal(8))
-	})
-
-	When("the write fails", func() {
-		It("returns an error", func() {
-
-			backingWriter := new(fakes.FakeWriter)
-			backingWriter.WriteReturns(0, errors.New("foo"))
-			logPercentageWriter := writer.NewLogPercentageWriter(backingWriter)
-
-			_, err := logPercentageWriter.Write(nil)
-			Expect(err).To(MatchError("foo"))
+		It("logs 33% on each write", func() {
+			Expect(fakeLogger.InfoCallCount()).To(Equal(0))
+			Expect(fakeWriter.WriteCallCount()).To(Equal(0))
+			logPercentageWriter.Write([]byte("words"))
+			Expect(fakeWriter.WriteCallCount()).To(Equal(1))
+			Expect(fakeLogger.InfoCallCount()).To(Equal(1))
+			cmd, message, args := fakeLogger.InfoArgsForCall(0)
+			Expect(cmd).To(Equal("schblam"))
+			Expect(message).To(ContainSubstring("message"))
+			Expect(args[0]).To(Equal(33))
 		})
 	})
 })
