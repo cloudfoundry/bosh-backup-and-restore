47631b3a90ea7abe05cb1465d555720ce12390e4
Neil Hickey <nhickey@pivotal.io>
2020-12-04 21:43:56 +0000

    Move bosh_all_proxy tests to deployment and director
    [#175938289]

--- system/director/backup_test.go
+++ system/director/backup_test.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
+
+	. "github.com/cloudfoundry-incubator/bosh-backup-and-restore/system"
 )
 
 var _ = Describe("Backup", func() {
@@ -24,6 +27,13 @@ var _ = Describe("Backup", func() {
 		})
 	})
 
+	boshAllProxy := fmt.Sprintf(
+		"ssh+socks5://%s@%s?private-key=%s",
+		MustHaveEnv("BOSH_GW_USER"),
+		MustHaveEnv("BOSH_GW_HOST"),
+		MustHaveEnv("BOSH_GW_PRIVATE_KEY"),
+	)
+
 	Context("when the operator specifies a valid artifact path", func() {
 		It("backs up the director", func() {
 			artifactDir, err := ioutil.TempDir("", "bbr_system_test_director")
@@ -38,6 +48,24 @@ var _ = Describe("Backup", func() {
 			mustHaveFile(backupDir, "bosh-0-amazing-backup-and-restore.tar")
 
 			Expect(os.RemoveAll(artifactDir)).To(Succeed())
+		})
+
+		It("backs up the director using BOSH_ALL_PROXY", func() {
+			cmd := exec.Command(
+				commandPath,
+				"director",
+				"--username", MustHaveEnv("DIRECTOR_SSH_USERNAME"),
+				"--private-key-path", MustHaveEnv("DIRECTOR_SSH_KEY_PATH"),
+				"--host", MustHaveEnv("DIRECTOR_ADDRESS"),
+				"pre-backup-check",
+			)
+			cmd.Env = append(os.Environ(), "BOSH_ALL_PROXY="+boshAllProxy)
+			cmd.Stderr = GinkgoWriter
+			cmd.Stdout = GinkgoWriter
+
+			fmt.Println("BOSH_ALL_PROXY=", boshAllProxy, " bbr ", cmd.Args)
+
+			Expect(cmd.Run()).To(Succeed())
 		})
 	})
 
