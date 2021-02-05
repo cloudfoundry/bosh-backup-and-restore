abd9ae56f17f6df0592477f5c8e2c1e47bcda264
Guillermo Bandres <gbandres@vmware.com>
2021-02-05 16:53:40 +0100

    Removed credentials from set-pipeline.sh
    [#176800367]

--- ci/pipelines/test-github-prs/set-pipeline.sh
+++ ci/pipelines/test-github-prs/set-pipeline.sh
@@ -2,19 +2,8 @@
 
 set -euo pipefail
 
-GITHUB_ACCESS_TOKEN=$(lpass show "Shared-Cryogenics/github-ci-access-token" --field="password")
-GITHUB_CI_SSH_KEY=$(lpass show "Shared-Cryogenics/github-ci-ssh-key" --field="Private Key")
-DOCKER_HOST_SSH_KEY=$(lpass show "Shared-Cryogenics/docker-host-ssh-key" --notes)
-TRACKER_API_TOKEN=$(lpass show "Shared-Cryogenics/tracker-api-token" --notes)
-TRACKER_PROJECT_ID=2475702
-
 fly --target cryo-bbr set-pipeline \
   --pipeline test-github-prs \
-  --config pipeline.yml \
-  --var github-access-token="${GITHUB_ACCESS_TOKEN}" \
-  --var github-ci-ssh-key="${GITHUB_CI_SSH_KEY}" \
-  --var docker-host-ssh-key="${DOCKER_HOST_SSH_KEY}" \
-  --var tracker-api-token="${TRACKER_API_TOKEN}" \
-  --var tracker-project-id="${TRACKER_PROJECT_ID}"
+  --config pipeline.yml
 
 
