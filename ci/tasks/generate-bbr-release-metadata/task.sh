f2fab8e68a8ae5a4dc729f0b3f6e829f10b0fd33
Diego Lemos <dlemos@vmware.com>
2021-09-17 12:52:22 +0100

    [ci] separate jobs for github and pivnet release
    [#179484418 ]

--- ci/tasks/generate-bbr-release-metadata/task.sh
+++ ci/tasks/generate-bbr-release-metadata/task.sh
@@ -1,60 +1,45 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
 
-ROOT_DIR="$PWD"
-PIVNET_FOLDER="${ROOT_DIR}/pivnet-release-with-metadata"
-REL_PIVNET_FOLDER="pivnet-release-with-metadata"
-GITHUB_FOLDER="${ROOT_DIR}/github-release-with-metadata"
-REL_GITHUB_FOLDER="github-release-with-metadata"
-RELEASE_FOLDER="${ROOT_DIR}/release"
-RELEASE_TAR_FOLDER="${ROOT_DIR}/release-tar"
-VERSION=$(cat "version-folder/${VERSION_PATH}")
+[ -d promoted-artefacts ]
+[ -d pivnet-artefacts ]
+[ -d repo ]
+
+: "${TEMPLATE_PATH:?}"
+: "${RELEASE_TYPE:?}"
+
+VERSION=$(cat "version/number")
 
 function main {
-  create_tarball
-  copy_tarball_to_folder "${GITHUB_FOLDER}"
-  copy_tarball_to_folder "${PIVNET_FOLDER}"
-  copy_release_files_to_folder "${GITHUB_FOLDER}"
-  delete_sha256_files
-  copy_release_files_to_folder "${PIVNET_FOLDER}"
-  export_release_metadata_variables
-
-  erb -T- "template-folder/${TEMPLATE_PATH}" > "${PIVNET_FOLDER}/release.yml"
-
-  echo -e "\n > Generated Tanzunet release file"
-  cat "${PIVNET_FOLDER}/release.yml"
+  copy_pivnet_files 'promoted-artefacts' 'pivnet-artefacts'
+  generate_pivnet_metadata "repo/${TEMPLATE_PATH}" 'pivnet-artefacts'
 }
 
-function create_tarball {
-  echo "Creating release tarball..."
-  export TAR_NAME="bbr-${VERSION}.tar"
-  tar -cf "${TAR_NAME}" -C "${RELEASE_TAR_FOLDER}" .
+function copy_pivnet_files() {
+  local promoted_artefacts="${1:?}"
+  local pivnet_artefacts="${2:?}"
+
+  ls "$promoted_artefacts"
+  cp "$promoted_artefacts"/* "$pivnet_artefacts" 
+
+  rm "$pivnet_artefacts"/*.sha256
 }
 
-function copy_tarball_to_folder {
-  echo "Adding tarball to: $1"
-  cp "${TAR_NAME}" "$1"
-}
+function generate_pivnet_metadata() {
+  local metadata_template="${1:?}"
+  local pivnet_artefacts="${2:?}"
 
-function delete_sha256_files {
-  echo "Removing sha256 files from tanzunet release"
-  # Why? The previous concourse job has generated shasums for each product,
-  # we have bundled this as part of the tar and do no need these extra files.
-  rm $RELEASE_FOLDER/*.sha256
-}
+  export BBR_LINUX_BINARY="$pivnet_artefacts/bbr-${VERSION}-linux-amd64"
+  export BBR_DARWIN_BINARY="$pivnet_artefacts/bbr-${VERSION}-darwin-amd64"
+  export RELEASE_TAR="$pivnet_artefacts/bbr-build/bbr-${VERSION}.tar"
+  export BBR_S3_VALIDATOR_BINARY="$pivnet_artefacts/bbr-s3-config-validator-${VERSION}-linux-amd64"
+  export BBR_S3_VALIDATOR_README="$pivnet_artefacts/bbr-s3-config-validator-${VERSION}.README.md"
+  export VERSION
+  export RELEASE_TYPE
 
-function copy_release_files_to_folder {
-  cp -r "${RELEASE_FOLDER}/." "$1"
-}
-
-function export_release_metadata_variables {
-  export BBR_LINUX_BINARY="${REL_PIVNET_FOLDER}/bbr-${VERSION}-linux-amd64"
-  export BBR_DARWIN_BINARY="${REL_PIVNET_FOLDER}/bbr-${VERSION}-darwin-amd64"
-  export RELEASE_TAR="${REL_PIVNET_FOLDER}/${TAR_NAME}"
-  export BBR_S3_VALIDATOR_BINARY="${REL_PIVNET_FOLDER}/bbr-s3-config-validator-${VERSION}-linux-amd64"
-  export BBR_S3_VALIDATOR_README="${REL_PIVNET_FOLDER}/bbr-s3-config-validator-${VERSION}.README.md"
-  export VERSION="${VERSION}"
+  erb -T- "$metadata_template" > "$pivnet_artefacts/release.yml"
 }
 
 main
