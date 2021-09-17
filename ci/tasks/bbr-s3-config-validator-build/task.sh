4cf2f33953a1db8ce04df85bb6ae495ea8efda2b
Diego Lemos <dlemos@vmware.com>
2021-09-17 17:25:25 +0100

    [ci] simplify s3-validator build task
    [#179484418 ]

--- ci/tasks/bbr-s3-config-validator-build/task.sh
+++ ci/tasks/bbr-s3-config-validator-build/task.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
+
+[ -d version ]
+[ -d repo ]
+
+VERSION="$(cat version/number)"
 
 ROOT_DIR=$PWD
 
-cd bosh-backup-and-restore/s3-config-validator
+cd repo/s3-config-validator
 
 make artifact
 
 cp -r \
   build/artifact.tgz \
-  "${ROOT_DIR}/bbr-s3-config-validator-test-artifacts/bbr-s3-config-validator.$(cat "${ROOT_DIR}"/s3-config-validator-version/number).tgz"
+  "${ROOT_DIR}/bbr-s3-config-validator-test-artifacts/bbr-s3-config-validator.$VERSION.tgz"
