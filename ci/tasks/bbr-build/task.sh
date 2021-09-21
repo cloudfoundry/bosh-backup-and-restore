d0c1cf47b999c408d4df99be9a9b5fa89641bf58
Diego Lemos <dlemos@vmware.com>
2021-09-21 11:08:07 +0100

    [bug] `bbr -v` now displays final version
    [fixes #179654999]

--- ci/tasks/bbr-build/task.sh
+++ ci/tasks/bbr-build/task.sh
@@ -2,14 +2,17 @@
 
 set -eu
 
-[ -d 'version' ]
+[ -d 'bosh-backup-and-restore' ]
+[ -d 'release-candidate-version' ]
+[ -d 'release-version' ]
 
 eval "$(ssh-agent)"
 echo -e "${GITHUB_SDK_PRIVATE_KEY}" > "${PWD}/github-sdk.key"
 chmod 400 "${PWD}/github-sdk.key"
 ssh-add "${PWD}/github-sdk.key"
 
-VERSION=$(cat version/number)
+RELEASE_CANDIDATE_VERSION=$(cat release-candidate-version/number)
+VERSION=$(cat release-version/number)
 export VERSION
 
 BBR_REPO="bosh-backup-and-restore"
@@ -25,22 +28,21 @@ echo "The release directory now contains the following files:"
 ls -R bbr-release
 
 echo "Creating release tarball..."
-tar -C bbr-release -cf "bbr-build/bbr-$VERSION.tar" .
-
+tar -C bbr-release -cf "bbr-build/bbr-$RELEASE_CANDIDATE_VERSION.tar" .
 echo "Auto-delivered in
-https://s3-eu-west-1.amazonaws.com/bosh-backup-and-restore-builds/bbr-$VERSION.tar
+https://s3-eu-west-1.amazonaws.com/bosh-backup-and-restore-builds/bbr-$RELEASE_CANDIDATE_VERSION.tar
 
 [Backup and Restore Bot]" > bbr-build/message
 
 echo "Moving linux binary to the build directory..."
 
-LINUX="bbr-$VERSION-linux-amd64"
+LINUX="bbr-$RELEASE_CANDIDATE_VERSION-linux-amd64"
 mv "$BBR_REPO"/releases/bbr bbr-build/"$LINUX"
 cat "$BBR_REPO"/releases/checksum.sha256 | cut -d' ' -f1  | sed -n '1p' > bbr-build/"$LINUX".sha256
 
 echo "Moving mac binary to the build directory..."
 
-DARWIN="bbr-$VERSION-darwin-amd64"
+DARWIN="bbr-$RELEASE_CANDIDATE_VERSION-darwin-amd64"
 mv "$BBR_REPO"/releases/bbr-mac bbr-build/"$DARWIN"
 cat "$BBR_REPO"/releases/checksum.sha256 | cut -d' ' -f1  | sed -n '2p' > bbr-build/"$DARWIN".sha256
 
