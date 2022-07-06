39e4c368f749399487978be23c53ece30043c531
Fernando Naranjo <fnaranjo@vmware.com>
2022-07-06 13:06:24 +0200

    Revert "Ensure fields have the right format"
    This reverts commit 785710ebaae9d20df7efb16aebd26f66b98dc541.

--- ci/tasks/bosh-deployment-resource-source-file-adapter/task.sh
+++ ci/tasks/bosh-deployment-resource-source-file-adapter/task.sh
@@ -5,12 +5,7 @@ if [[ -z "${BOSH_GW_USER}" ]] && [[ -z "${BOSH_GW_HOST}" ]] && [[ -n "${BOSH_ALL
     export BOSH_GW_USER="$(echo "${BOSH_ALL_PROXY}" | sed -nr 's/^.*:\/\/([^@]+)@([^?]+)?.*$/\1/p')"
     export BOSH_GW_HOST="$(echo "${BOSH_ALL_PROXY}" | sed -nr 's/^.*:\/\/([^@]+)@([^?]+)?.*$/\2/p')"
 fi
-
-if [[ "${BOSH_GW_HOST}" != *":"* ]]; then
-    export BOSH_GW_HOST="${BOSH_GW_HOST}:22"
-fi
-
-cat <<EOF> source-file/source-file.yml
+yq write <( cat <<EOF
 ---
 jumpbox_username: ${BOSH_GW_USER}
 jumpbox_url: ${BOSH_GW_HOST}
@@ -18,6 +13,7 @@ target: ${BOSH_ENVIRONMENT}
 client: ${BOSH_CLIENT}
 client_secret: ${BOSH_CLIENT_SECRET}
 EOF
+) -- "ca_cert" "$BOSH_CA_CERT" > source-file/source-file.yml
 
-yq write --inplace -- source-file/source-file.yml ca_cert "$(sed -E 's/(-+(BEGIN|END) CERTIFICATE-+) *| +/\1\n/g' <<< "$BOSH_CA_CERT")"
-yq write --inplace -- source-file/source-file.yml jumpbox_ssh_key "$(sed -E 's/(-+(BEGIN|END) RSA PRIVATE KEY-+) *| +/\1\n/g' <<< "$BOSH_GW_PRIVATE_KEY")"
+yq write --inplace -- source-file/source-file.yml jumpbox_ssh_key "$BOSH_GW_PRIVATE_KEY"
+
