effae55d1127a651557bf124b2b3991e686f9e62
fnaranjo <fnaranjo@vmware.com>
2022-07-06 10:43:38 +0200

    Establish tunnel to Director via Jumpbox
    #182318242

--- ci/tasks/bbr-system-deployment/task.sh
+++ ci/tasks/bbr-system-deployment/task.sh
@@ -10,6 +10,23 @@ ssh-add "${PWD}/github-sdk.key"
 echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
 chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+
+ssh-add "$BOSH_GW_PRIVATE_KEY"
+
+export DIRECTOR_HOST="$(echo "$BOSH_ENVIRONMENT" | sed -E 's/(https:\/\/)?([^:]*)(:.*)?/\2/g')"
+export DIRECTOR_PORT="$(echo "$BOSH_ENVIRONMENT" | sed -E 's/(https:\/\/)?([^:]*)(:.*)?/\3/g')"
+
+sshuttle -r "${BOSH_GW_USER}@${BOSH_GW_HOST}" "$DIRECTOR_HOST/32$DIRECTOR_PORT" \
+  --daemon \
+  -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=600'
+echo "Establishing tunnel to Director via Jumpbox..."
+sleep 5
+
+if ! stat sshuttle.pid > /dev/null 2>&1; then
+  echo "Failed to start sshuttle daemon"
+  exit 1
+fi
+
 export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 cd bosh-backup-and-restore
