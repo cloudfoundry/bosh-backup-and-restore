848fcacfa37fcab40b84e2bafdd5aff9e1e4248b
Diego Lemos <dlemos@vmware.com>
2023-03-04 10:57:50 +0000

    [ci] remove ginkgo installation + bosh-cli
    - Those are now available in the cryogenics/essentials image

--- ci/tasks/bbr-system-deployment/task.sh
+++ ci/tasks/bbr-system-deployment/task.sh
@@ -12,10 +12,5 @@ chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
 export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
-GOBIN=/usr/local/bin go install github.com/onsi/ginkgo/ginkgo@latest
-ginkgo version
-
-ln -s "$( which bosh )" /usr/local/bin/bosh-cli
-
 cd bosh-backup-and-restore
 make sys-test-deployment-ci
