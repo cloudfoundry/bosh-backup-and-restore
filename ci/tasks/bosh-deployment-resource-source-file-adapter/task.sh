46d28e74eef8f3c8d74d8489609c80ea08149be3
Konstantin <34882943+nouseforaname@users.noreply.github.com>
2024-03-21 22:12:34 +0100

    remove usage of harbor, implement adr-006 (#1434)
    * remove usage of harbor, implement adr-006
    
    [#187074138]
    
    remove usage of harbor and ensure each task has an image attribute set
    so we can manage ci task images as concourse resources.
    
    fly-by:
    - replace docker-image with registry-imgae
    - converge on cryogenics-essentials where possible
    
    * switch task to https://github.com/kislyuk/yq
    
    this task now uses cryo essentials image and that image has the
    `other` yq

--- ci/tasks/bosh-deployment-resource-source-file-adapter/task.sh
+++ ci/tasks/bosh-deployment-resource-source-file-adapter/task.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-yq write <( cat <<EOF
+cat <<EOF> source-file/source-file.yml
 ---
 jumpbox_username: ${BOSH_GW_USER}
 jumpbox_url: ${BOSH_GW_HOST}
 target: ${BOSH_ENVIRONMENT}
 client: ${BOSH_CLIENT}
 client_secret: ${BOSH_CLIENT_SECRET}
+ca_cert:
 EOF
-) -- "ca_cert" "$BOSH_CA_CERT" > source-file/source-file.yml
-
-yq write --inplace -- source-file/source-file.yml jumpbox_ssh_key "$BOSH_GW_PRIVATE_KEY"
-
+yq -iy ".ca_cert=\"$BOSH_CA_CERT\"" source-file/source-file.yml
+yq -iy ".jumpbox_ssh_key=\"$BOSH_GW_PRIVATE_KEY\"" source-file/source-file.yml
