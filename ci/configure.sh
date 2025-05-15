843e0e72cd896a8ad6d125ebb752478baa71a91b
aram price <aram.price@broadcom.com>
2025-05-15 15:26:11 -0700

    CI: add worker tags
    - fix configure script

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,4 +5,4 @@ REPO_ROOT="$( cd "$( dirname "${0}" )/.." && pwd )"
 
 fly -t "${CONCOURSE_TARGET:=bosh-ecosystem}" set-pipeline \
   -p bbr-cli \
-  -c <(ytt -f "${REPO_ROOT}/ci/pipeline.yml" --data-values-file "${REPO_ROOT}ci/pipeline-values.yml")
+  -c <(ytt -f "${REPO_ROOT}/ci/pipeline.yml" --data-values-file "${REPO_ROOT}/ci/pipeline-values.yml")
