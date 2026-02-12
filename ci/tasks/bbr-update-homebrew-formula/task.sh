32d9a334cedf7b3ee8b7e0e3981df0e7f05c0996
aram price <aram.price@broadcom.com>
2026-02-11 16:16:41 -0800

    CI: fix homebrew tap updater job
    Co-authored-by: Chris Selzo <chris.selzo@broadcom.com>

--- ci/tasks/bbr-update-homebrew-formula/task.sh
+++ ci/tasks/bbr-update-homebrew-formula/task.sh
@@ -58,7 +58,6 @@ EOF
 
   git add bbr.rb
   git config --global user.name "PCF Backup & Restore CI"
-  git config --global user.email "cf-lazarus@pivotal.io"
   if git commit -m "Release BBR CLI v${VERSION}"; then
     echo "Updated homebrew formula to bbr v${VERSION}"
   else
